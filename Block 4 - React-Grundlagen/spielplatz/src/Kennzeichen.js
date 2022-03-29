a028e8c1b4df5a3ee6b0f41e463046526f62567f
Max Beier <max.beier@finnoconsult.at>
2022-03-29 09:50:34 +0200

    update kennzeichen app

--- Block 4 - React-Grundlagen/spielplatz/src/Kennzeichen.js
+++ Block 4 - React-Grundlagen/spielplatz/src/Kennzeichen.js
@@ -1,14 +1,20 @@
+import { useState } from 'react';
 import kennzeichen from './kennzeichen.json';
 
 export function Kennzeichen() {
+  const [query, setQuery] = useState('');
+  const handleChange = (event) => setQuery(event.target.value.toLowerCase());
   const resultKeys = Object.keys(kennzeichen);
+  const filteredKeys = resultKeys.filter(
+    (key) => key.toLowerCase().startsWith(query) || kennzeichen[key].toLowerCase().startsWith(query)
+  );
 
   return (
     <div className="container">
       <h1>Kennzeichen</h1>
-      <input className="form-control" placeholder="Suchbegriff" />
+      <input className="form-control" placeholder="Suchbegriff" onChange={handleChange} />
       <ul className="list-group mt-2">
-        {resultKeys.map((key) => (
+        {filteredKeys.map((key) => (
           <li key={key} className="list-group-item">
             <strong>{key}</strong>: <span>{kennzeichen[key]}</span>
           </li>
