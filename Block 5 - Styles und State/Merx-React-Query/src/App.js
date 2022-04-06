006382ff3bc46eaaf2e2b4f532c22040dff123e6
Max Beier <max.beier@finnoconsult.at>
2022-04-06 10:12:21 +0200

    Merx-React-Query: add react-query

--- Block 5 - Styles und State/Merx-React-Query/src/App.js
+++ Block 5 - Styles und State/Merx-React-Query/src/App.js
@@ -1,4 +1,6 @@
 import { Provider as ReduxProvider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
@@ -10,25 +12,41 @@ import { Login } from './Login';
 import { theme } from './theme';
 import { store, persistor } from './store/store';
 
+const loadFromServer = ({ queryKey }) => fetch(`http://localhost:3001/${queryKey[0]}`).then((res) => res.json());
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      queryFn: loadFromServer,
+    },
+  },
+});
+
 export function App() {
   return (
     <BrowserRouter>
-      <ReduxProvider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-            <Header />
-            <Container as="main" maxWidth="lg">
-              <Routes>
-                <Route path="/" element={<Products />} />
-                <Route path="/products/:id" element={<Product />} />
-                <Route path="/login" element={<Login />} />
-              </Routes>
-            </Container>
-          </ThemeProvider>
-        </PersistGate>
-      </ReduxProvider>
+      <QueryClientProvider client={queryClient}>
+        <ReduxProvider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <ThemeProvider theme={theme}>
+              <CssBaseline />
+              <link
+                rel="stylesheet"
+                href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+              />
+              <Header />
+              <Container as="main" maxWidth="lg">
+                <Routes>
+                  <Route path="/" element={<Products />} />
+                  <Route path="/products/:id" element={<Product />} />
+                  <Route path="/login" element={<Login />} />
+                </Routes>
+              </Container>
+            </ThemeProvider>
+          </PersistGate>
+        </ReduxProvider>
+        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+      </QueryClientProvider>
     </BrowserRouter>
   );
 }
--- Block 5 - Styles und State/Merx-React-Query/src/Products.js
+++ Block 5 - Styles und State/Merx-React-Query/src/Products.js
@@ -1,17 +1,9 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useQuery } from 'react-query';
 import { Grid, Box, CircularProgress, Alert, AlertTitle } from '@mui/material';
 import { ProductCard } from './ProductCard';
-import { loadProducts } from './store/actions';
 
 export function Products() {
-  const dispatch = useDispatch();
-  const { isLoading, data, error } = useSelector((state) => state.products);
-  const productIds = Object.keys(data);
-
-  useEffect(() => {
-    if (!isLoading && productIds.length === 0) dispatch(loadProducts());
-  }, []);
+  const { isLoading, error, data } = useQuery('products');
 
   if (isLoading)
     return (
@@ -28,13 +20,13 @@ export function Products() {
       </Alert>
     );
 
-  if (productIds.length === 0) return <h1>No Products</h1>;
+  if (data.data.length === 0) return <h1>No Products</h1>;
 
   return (
     <Grid container spacing={4} mt={0} mb={4}>
-      {productIds.map((id) => (
-        <Grid item key={id} xs={12} sm={6} md={4}>
-          <ProductCard key={id} {...data[id]} />
+      {data.data.map((product) => (
+        <Grid item key={product.id} xs={12} sm={6} md={4}>
+          <ProductCard key={product.id} {...product} />
         </Grid>
       ))}
     </Grid>
